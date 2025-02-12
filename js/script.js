1953cdebe8afd412baba3d4119827c7003141aff
AidaMontes <KJS05884@educastur.es>
2025-02-12 20:57:52 +0100

    Revert "corregido problema en script.js rama main"
    This reverts commit f3a5aefa58d5719729e9dee4f2df413f099d97ba.

--- js/script.js
+++ js/script.js
@@ -22,10 +22,5 @@ document.addEventListener("DOMContentLoaded", function() {
         if (!valid) {
             event.preventDefault();
         }
-        
-        if (mensaje.value.trim() === "") {
-            alert("Ingrese un mensaje, por favor");
-            valid = false;
-        }
     });
 });
